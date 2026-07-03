bdcbd211ded4b6a5c2375b83a9c4504012cf6200
Felipe Mendes <luizfelipemendes20@gmail.com>
2026-07-03 15:37:14 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("leadForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const telefone = document.getElementById("telefone").value;
+  const idade = document.getElementById("idade").value;
+  const plano = document.getElementById("tipoPlano").value;
+
+  const mensagem = `
+Olá, me chamo ${nome}.
+Tenho ${idade} anos.
+Quero cotar plano ${plano}.
+Meu WhatsApp: ${telefone}
+  `;
+
+  const numeroWhatsApp = "55SEUNUMEROAQUI";
+
+  const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+
+  window.open(url, "_blank");
+});
