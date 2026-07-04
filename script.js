da6603a61a7ef528841b5b33985562e192d586d6
Felipe Mendes <luizfelipemendes20@gmail.com>
2026-07-03 23:39:06 -0300

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-document.getElementById("leadForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
+<script>
+function enviar() {
   const nome = document.getElementById("nome").value;
-  const telefone = document.getElementById("telefone").value;
+  const tel = document.getElementById("tel").value;
   const idade = document.getElementById("idade").value;
-  const plano = document.getElementById("tipoPlano").value;
+  const plano = document.getElementById("plano").value;
 
-  const mensagem = `Olá, me chamo ${nome}. Tenho ${idade} anos. Quero cotar o plano ${plano}. Meu WhatsApp é ${telefone}.`;
+  const msg = `Olá, me chamo ${nome}. Tenho ${idade} anos. Quero cotar o plano ${plano}. Meu WhatsApp é ${tel}.`;
 
-  const numeroWhatsApp = "5521965768924";
+  const numeroWhatsApp = "5521965768924"; // SEU NÚMERO AQUI
 
-  const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+  const url = "https://wa.me/" + numeroWhatsApp + "?text=" + encodeURIComponent(msg);
 
- window.location.href = url;
-});
+  window.location.href = url;
+}
+</script>
